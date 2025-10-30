7f743098f8d6e4690f6ca9ce740a9ade0073c9f8
Giovanni León <giovannilc.dev@gmail.com>
2025-10-30 14:22:32 +0000

    allow indexed Transport

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,3 +1,5 @@
 export * from './types/Level.type';
 export * from './types/Log.type';
 export * from './Logger';
+export * from './Transport/Transport'
+export * from './Transport/ConsoleTransport'
